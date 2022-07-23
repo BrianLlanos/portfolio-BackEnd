8bc7cf3eb6c10ced710b5742f2e283c24a5e627e
BrianLlanos <brianllanosbuzon@gmail.com>
2022-07-22 23:41:49 -0300

    Cambios en el programa de prueba

--- programaDePrueba.py
+++ programaDePrueba.py
@@ -1,4 +1,11 @@
-//Solo es un archivo para subir a mi repositorio de backend en github
+#solo es un archivo para subir a mi repositorio de backend en github
 
 print("Archivo de prueba")
 
+#agrego una nueva linea para ver si cambia en algo el commit
+
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
+edad = int(input("Ingrese su edad: "))
+
+print("Sus datos ingresados", nombre, apellido, "de", edad, "años")
